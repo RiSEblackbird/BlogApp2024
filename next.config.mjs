2586128e05865478e01400f22c15c3ef03d54701
taishi.senoo <mokut2017@gmail.com>
2024-10-01 01:23:31 +0900

    Implement secure image serving for submodule content (要精読)

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['blog-app2024-1mkyidmp1-areyakoreya.vercel.app'],
-    },
-    // 静的画像を扱うための設定
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]',
-            },
-          },
-        ],
-      });
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'blog-app2024-1mkyidmp1-areyakoreya.vercel.app'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/posts/images/:path*',
+        destination: '/api/images/:path*',
+      },
+    ]
+  },
+}
+
+export default nextConfig

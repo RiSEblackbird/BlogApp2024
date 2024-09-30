53b0a1c016169e63faa708cf1134dd26b645f18c
taishi.senoo <mokut2017@gmail.com>
2024-10-01 01:11:16 +0900

    fix next.config.mjs for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['blog-app2024-1mkyidmp1-areyakoreya.vercel.app'],
+    },
+    // 静的画像を扱うための設定
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: '/_next',
+              name: 'static/media/[name].[hash].[ext]',
+            },
+          },
+        ],
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
